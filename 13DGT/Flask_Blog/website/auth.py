d6dc0722d9ee3190b5334419d163603852537d8f
Ty-Tupai <24257@my.sanctamaria.school.nz>
2025-07-30 09:57:29 +1200

    Digital Website
    Completed Schoology videos from 1-17.

---
+++ 13DGT/Flask_Blog/website/auth.py
@@ -0,0 +1,89 @@
+
+# import external libraries
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from flask_login import login_user, logout_user, login_required, current_user
+from werkzeug.security import generate_password_hash, check_password_hash
+#import database
+from . import db
+#import from .models user
+from .models import User
+
+# set auth blueprint
+auth = Blueprint("auth", __name__)
+
+
+
+# sign-up route
+@auth.route("/sign-up", methods=['GET', 'POST'])
+# sign up function
+# returns sign up page
+def sign_up():
+    # signup function
+    # returns signup page
+    if request.method == 'POST':
+        email = request.form.get("email")
+        username = request.form.get("username")
+        password1 = request.form.get("password1")
+        password2 = request.form.get("password2")
+        # check that email and username are unique
+        email_exists = User.query.filter_by(email=email).first()
+        username_exists = User.query.filter_by(username=username).first()
+        # validation of password, username and email
+        if email_exists:
+            flash('Emial is already in use.' , category='error')
+        elif username_exists:
+            flash('Username is already in use.' , category='error')
+        elif password1 != password2:
+            flash('Passwords do not match!' , category='error')
+        elif len(password1) < 8:
+            flash('Password is too short.' , category='error')
+        elif len(username) < 2:
+            flash('Username is too short.' , category='error')
+        elif len(email) < 4:
+            flash('Email is not valid.' , category='error')
+        else:
+            new_user = User(email=email, username=username, password=generate_password_hash(password1, method='scrypt:32768:8:1'))
+            db.session.add(new_user)
+            db.session.commit()
+            login_user(new_user, remember=True)
+            flash('Your account has been created!', category= 'success')
+            return redirect(url_for('views.home'))
+
+    return render_template("sign_up.html", user=current_user)
+
+# login route
+@auth.route("/login", methods=['GET', 'POST'])
+# login function
+# returns login page
+def login():
+    # gets email and password from login form
+    if request.method == 'POST':
+        email = request.form.get("email")
+        password = request.form.get("password")
+        # queries database to recieve user information using email address
+        user = User.query.filter_by(email=email).first()
+        # checks email and password
+        if user:
+            # if correct log in user and redirect to home page
+            if check_password_hash(user.password, password):
+                flash("Logged In!", category="success")
+                login_user(user, remember=True)
+                return redirect(url_for('views.home'))
+            # if incorrect password flash error
+            else:
+                flash("Incorrect Password!", category="error")
+        # if incorrect email flash error
+        else:
+            flash("Email does not exist!", category="error")
+        
+        
+    return render_template("login.html", user=current_user)
+
+# logout route
+@auth.route("/logout")
+@login_required
+# logout function
+# returns logout page
+def logout():
+    logout_user()
+    return redirect(url_for('views.home'))
---
+++ 13DGT/Flask_Blog/website/views.py
@@ -0,0 +1,68 @@
+from flask_login import login_user, logout_user, login_required, current_user
+
+# import external libraries
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+#import database
+from . import db
+#import from .models user
+from .models import User, Post
+# set views blueprint
+views = Blueprint("views", __name__)
+
+# default/home route
+@views.route("/")
+@views.route("/home")
+# home route function
+# returns home.html
+def home():
+    return render_template("home.html", user=current_user)
+
+
+# blog page route
+@views.route("/blog")
+# user must be logged in to post
+@login_required
+# home route function
+# returns home.html
+def blog():
+    posts = Post.query.all()
+    return render_template("blog.html", user=current_user, posts=posts)
+
+
+# create blog post route
+@views.route("/create-post", methods=['GET','POST'])
+# user must be logged in to post
+@login_required
+def create_post():
+    if request.method == "POST":
+        title = request.form.get('title')
+        content = request.form.get('content')
+        if not title:
+            flash('title cannot be empty', category='error')
+        elif not content:
+            flash('Post cannot be empty', category='error')
+        else:
+            post = Post(title=title, content=content, author=current_user.id)
+            db.session.add(post)
+            db.session.commit()
+            flash('Post created!', category= 'success')
+        return redirect(url_for('views.blog'))
+        
+    return render_template("create_post.html", user=current_user)
+
+
+# delete blog post route
+@views.route("/delete-post/<id>")
+# user must be logged in to post
+@login_required
+def delete_post(id):
+    post = Post.query.filter_by(id=id).first()
+    if not post:
+        flash('Post does not exist', category='error')
+    elif current_user.id != post.author:
+        flash('You do not have permission to delete this post', category='error')
+    else:
+        db.session.delete(post)
+        db.session.commit()
+        flash('Post deleted!', category= 'success')
+    return redirect(url_for('views.blog'))
